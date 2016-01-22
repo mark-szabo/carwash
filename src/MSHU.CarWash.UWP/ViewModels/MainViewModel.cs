f7f13c3fb553229551ee8dec8494f70cc5cc08ae
tveiland <tamas.veiland@hotmail.com>
2016-01-22 21:37:49 +0100

    Basic project architecture added.
    Related Work Items: #29

---
+++ src/MSHU.CarWash.UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSHU.CarWash.UWP.ViewModels
+{
+    public class MainViewModel : Bindable
+    {
+        /// <summary>
+        /// Gets or sets the command that handles the app registrations.
+        /// </summary>
+        public RelayCommand RegisterCommand { get; set; }
+
+        /// <summary>
+        /// Default constructor initializes basic business logic.
+        /// </summary>
+        public MainViewModel()
+        {
+            RegisterCommand = new RelayCommand(this.ExecuteRegisterCommand);
+        }
+
+        /// <summary>
+        /// Event handler for the Executed event of the RegisterCommand.
+        /// </summary>
+        private void ExecuteRegisterCommand(object param)
+        {
+
+        }
+
+    }
+}
