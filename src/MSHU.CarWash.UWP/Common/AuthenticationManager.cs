1a0cf4be8f1c4c230c419586d7a597b6ec10f942
József Zoltán Vadkerti <jozsef.vadkerti@hotmail.com>
2016-02-12 13:58:44 +0100

    Access to mobileservice through ADAL, since MobileServiceClient doesn't work as expected

--- src/MSHU.CarWash.UWP/Common/AuthenticationManager.cs
+++ src/MSHU.CarWash.UWP/Common/AuthenticationManager.cs
@@ -15,7 +15,7 @@ namespace MSHU.CarWash.UWP.Common
     {
         //vadkertimobiletestnativeapp's properies in Azure AD
         private string m_ClientId = "1d316939-3200-4b05-9072-a5c92ae8c5a0";
-        private Uri m_RedirectUri = new Uri("ms-app://s-1-15-2-348789351-3529148773-2918319933-3807175127-3638082815-3054471230-807679675/");
+        private Uri m_AppUri = new Uri("ms-app://s-1-15-2-348789351-3529148773-2918319933-3807175127-3638082815-3054471230-807679675/");
 
         //Session to Azure AD
         private const string s_TenantId = "microsoft.onmicrosoft.com";
@@ -45,7 +45,7 @@ namespace MSHU.CarWash.UWP.Common
             // To determine this application's redirect URI, which is necessary when registering the app
             // in AAD, set a breakpoint on the next line, run the app, and copy the string value of the URI.
             // This is the only purposes of this line of code, it has no functional purpose in the application.
-            this.m_RedirectUri = 
+            this.m_AppUri = 
                 Windows.Security.Authentication.Web.WebAuthenticationBroker.GetCurrentApplicationCallbackUri();
 
         }
@@ -61,43 +61,43 @@ namespace MSHU.CarWash.UWP.Common
         {
             bool success = false;
 
-            MobileServiceClient client = 
+            /*MobileServiceClient client = 
                 new MobileServiceClient("https://vadkertitestmobile.azurewebsites.net");
             MobileServiceUser user
-                = await client.LoginAsync(MobileServiceAuthenticationProvider.WindowsAzureActiveDirectory, true);
+                = await client.LoginAsync(MobileServiceAuthenticationProvider.WindowsAzureActiveDirectory);
 
             IMobileServiceTable table = client.GetTable("TodoItem");
-            JToken result = await table.ReadAsync("select * from TodoItem");
-            
-            //AuthenticationResult result = await m_AuthContext.AcquireTokenAsync(
-            //    "https://vadkertitestmobile.azurewebsites.net",
-            //    m_ClientId,
-            //    m_RedirectUri,
-            //    PromptBehavior.Auto);
-            //if (result.Status != AuthenticationStatus.Success)
-            //{
-            //    if (result.Error == "authentication_canceled")
-            //    {
-            //        // The user cancelled the sign-in, no need to display a message.
-            //    }
-            //    else
-            //    {
-            //        //pop up the error message
-            //        Windows.UI.Popups.MessageDialog dialog =
-            //            new Windows.UI.Popups.MessageDialog(string.Format("If the error continues, please contact your administrator.\n\nError: {0}\n\nError Description:\n\n{1}", result.Error, result.ErrorDescription), "Sorry, an error occurred while signing you in.");
-            //        await dialog.ShowAsync();
-            //    }
-            //}
-            //else
-            //{
-            //    //successful sign in
-            //    IsUserAuthenticated = true;
-            //    UserData = result.UserInfo;
-            //    success = true;
-            //    BearerAccessToken = result.AccessToken;
-            //    //if this called then the webview needs no authentication at all
-            //    await ReadValues(BearerAccessToken);
-            //}
+            JToken result = await table.ReadAsync("select * from TodoItem");*/
+
+            AuthenticationResult result = await m_AuthContext.AcquireTokenAsync(
+                "https://vadkertitestmobile.azurewebsites.net",
+                m_ClientId,
+                m_AppUri,
+                PromptBehavior.Auto);
+            if (result.Status != AuthenticationStatus.Success)
+            {
+                if (result.Error == "authentication_canceled")
+                {
+                    // The user cancelled the sign-in, no need to display a message.
+                }
+                else
+                {
+                    //pop up the error message
+                    Windows.UI.Popups.MessageDialog dialog =
+                        new Windows.UI.Popups.MessageDialog(string.Format("If the error continues, please contact your administrator.\n\nError: {0}\n\nError Description:\n\n{1}", result.Error, result.ErrorDescription), "Sorry, an error occurred while signing you in.");
+                    await dialog.ShowAsync();
+                }
+            }
+            else
+            {
+                //successful sign in
+                IsUserAuthenticated = true;
+                UserData = result.UserInfo;
+                success = true;
+                BearerAccessToken = result.AccessToken;
+                //if this called then the webview needs no authentication at all
+                await ReadValues(BearerAccessToken);
+            }
             return success;
         }
 
@@ -134,7 +134,7 @@ namespace MSHU.CarWash.UWP.Common
                 new AuthenticationHeaderValue("Bearer", token);
 
             // Call the Web API to get the values
-            Uri requestURI = new Uri("https://vadkertitestmobile.azurewebsites.net");
+            Uri requestURI = new Uri("https://vadkertitestmobile.azurewebsites.net/Tables/TodoItem?ZUMO-API-VERSION=2.0.0");
             HttpResponseMessage httpResponse = await httpClient.GetAsync(requestURI);
             if (httpResponse.IsSuccessStatusCode)
             {
