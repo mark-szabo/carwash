54b9b930b1874e3b7e0b0e3b52c45d06bc6bac43
Tamas Veiland <taveilan@microsoft.com>
2016-03-21 10:28:43 +0100

    BooleanToVisibilityConverter added.
    Related Work Items: #55

---
+++ src/MSHU.CarWash.UWP/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace MSHU.CarWash.UWP.Converters
+{
+
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Visible;
+        }
+    }
+}
