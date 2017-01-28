b468f2dfb57e9739b5fc84efabc419a95441dd53
Ákos Szegő <akos.szego@live.com>
2017-01-28 08:41:16 +0100

    Created interface for settings store
    Related Work Items: #533

---
+++ src/MSHU.CarWash.UWP/Interfaces/ISettingsStoreService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSHU.CarWash.UWP.Interfaces
+{
+    interface ISettingsStoreService
+    {
+        Task StoreSettingAsync<T>(string name, T setting);
+        Task<T> RetriveSettingAsync<T>(string name);
+    }
+}
