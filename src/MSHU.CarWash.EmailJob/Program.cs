102299404bf9091e397a7c03ad6de53a1b3a5310
József Zoltán Vadkerti <jozsef.vadkerti@hotmail.com>
2017-09-01 09:48:30 +0000

    Merged PR 10: Merge Dev_EmailSendingWebJob to Dev

---
+++ src/MSHU.CarWash.EmailJob/Program.cs
@@ -0,0 +1,23 @@
+﻿using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSHU.CarWash.EmailJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = DatabaseManager.GetReservation(DateTime.UtcNow.Date);
+            EmailAddress address = new EmailAddress("jozsef.vadkerti@microsoft.com");
+            EmailAddress addressOrg = new EmailAddress("a-libill@microsoft.com");
+            EmailAddress addressWash = new EmailAddress("@microsoft.com");
+            EmailManager.SendMessage(new List<EmailAddress>() {addressOrg, address}, "Mai foglalások", message, message).Wait();
+
+            Console.WriteLine(message);
+        }
+    }
+}
