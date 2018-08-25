917509d3eaf1246cfbebbede2bf2cad44a8d619d
Mark Szabo <v-maszab@microsoft.com>
2018-08-25 20:50:22 +0200

    Microsoft Store wrapper app updates

--- src/MSHU.CarWash.PWA.Windows/msapp-error.js
+++ src/MSHU.CarWash.PWA.Windows/msapp-error.js
@@ -1,22 +1,28 @@
-﻿(function () { 
-  var validParameterNames = [ "httpStatus", "failureName", "failureUrl" ]; 
-  
-  function parseQueryParameters() { 
-    var query = location.search.slice(1); 
-    return query.split("&").reduce(function (queryParameters, rawPair) {
-                                   var pair = rawPair.split("=").map(decodeURIComponent);
-                                   queryParameters[pair[0]] = pair[1];
-                                   return queryParameters;
-    }, {});
-  }
- 
-  function initialize() {
-    var queryParameters = parseQueryParameters();
-    validParameterNames.forEach(function (parameterName) {
-                               var parameterValue = queryParameters[parameterName] || "N/A"; 
-                               document.getElementById(parameterName + "Value").textContent = parameterValue;
-    });
-  }
-  
-  document.addEventListener("DOMContentLoaded", initialize);
+﻿(function () {
+    var validParameterNames = ['httpStatus', 'failureName', 'failureUrl'];
+
+    function parseQueryParameters() {
+        var query = location.search.slice(1);
+        return query.split('&').reduce(function (queryParameters, rawPair) {
+            var pair = rawPair.split('=').map(decodeURIComponent);
+            queryParameters[pair[0]] = pair[1];
+            return queryParameters;
+        }, {});
+    }
+
+    function initialize() {
+        var queryParameters = parseQueryParameters();
+        var errorInfo = 'timestamp: ' + (new Date()).toJSON() + ' \n';
+        errorInfo += 'online: ' + navigator.onLine + ' \n';
+        validParameterNames.forEach(function (parameterName) {
+            var parameterValue = queryParameters[parameterName] || 'N/A';
+            errorInfo += parameterName + ': ' + parameterValue + ' \n';
+        });
+
+        document.getElementById('errorinfo').value = errorInfo;
+
+        if (!navigator.onLine) document.getElementById('errormessage').textContent = 'You are offline.';
+    }
+
+    document.addEventListener('DOMContentLoaded', initialize);
 }());
