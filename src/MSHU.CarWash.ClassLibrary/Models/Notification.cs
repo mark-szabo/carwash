c41e9e9f1513e86f874b6e108ff65b9993fd1e72
Mark Szabo <v-maszab@microsoft.com>
2018-09-02 17:07:41 +0200

    Adding Notification model class

---
+++ src/MSHU.CarWash.ClassLibrary/Models/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MSHU.CarWash.ClassLibrary.Models
+{
+    /// <summary>
+    /// <see href="https://notifications.spec.whatwg.org/#dictdef-notificationoptions">Notification API Standard</see>
+    /// </summary>
+    public class Notification
+    {
+        public string Lang { get; set; }
+        public string Body { get; set; }
+        public string Tag { get; set; }
+        public string Image { get; set; }
+        public string Icon { get; set; }
+        public string Badge { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+        public bool RequireInteraction { get; set; } = true;
+        public List<NotificationAction> Actions { get; set; }
+    }
+
+    /// <summary>
+    /// <see href="https://notifications.spec.whatwg.org/#dictdef-notificationaction">Notification API Standard</see>
+    /// </summary>
+    public class NotificationAction
+    {
+        public string Action { get; set; }
+        public string Title { get; set; }
+    }
+}
