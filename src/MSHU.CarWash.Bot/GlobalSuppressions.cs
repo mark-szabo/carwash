96e469b8ba245203194741e740af0fea707ce75c
Mark Szabo <a-marks@microsoft.com>
2018-12-21 23:30:42 +0100

    Upgrading the bot to ASP.NET Core 2.2

--- src/MSHU.CarWash.Bot/GlobalSuppressions.cs
+++ src/MSHU.CarWash.Bot/GlobalSuppressions.cs
@@ -1,10 +1,8 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
+﻿// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:Braces should not be omitted", Justification = "Bullshit")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1128:Put constructor initializers on their own line", Justification = "why the heck would I?")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1118:Parameter should not span multiple lines", Justification = "because just")]
-
