042cea786b84a0cddf66c43415ace2b21b4b900c
Mark Szabo <a-marks@microsoft.com>
2019-01-19 21:17:35 +0100

    Scaffolding PWA.Tests project

---
+++ CarWash.PWA.Tests/ApiTests/CalendarServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using CarWash.PWA.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using Xunit;
+
+namespace CarWash.PWA.Tests.ApiTests
+{
+    public class CalendarServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .AddJsonFile("appsettings.Development.json", true)
+                .AddEnvironmentVariables().Build();
+            var calendarService = new CalendarService(configuration);
+        }
+    }
+}
