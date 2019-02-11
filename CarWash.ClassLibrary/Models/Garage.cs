74e2d8ef73e28ce8b0c8a483bbe8fd0aaf8cdd3f
Mark Szabo <a-marks@microsoft.com>
2019-02-11 01:38:08 +0100

    .well-known endpoint

---
+++ CarWash.ClassLibrary/Models/Garage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CarWash.ClassLibrary.Models
+{
+    /// <summary>
+    /// Representation of a parking garage where cars are allowed to be left.
+    /// </summary>
+    public class Garage
+    {
+        /// <summary>
+        /// Gets or sets the building name where the parking garage is located.
+        /// </summary>
+        public string Building { get; set; }
+
+        /// <summary>
+        /// Gets or sets the floors of the parking garage.
+        /// </summary>
+        public List<string> Floors { get; set; }
+    }
+}
---
+++ CarWash.PWA/Controllers/WellKnownController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CarWash.ClassLibrary.Models;
+
+namespace CarWash.PWA.Controllers
+{
+    /// <summary>
+    /// .well-known API
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/.well-known")]
+    [ApiController]
+    public class WellKnownController : ControllerBase
+    {
+        private readonly CarWashConfiguration _configuration;
+
+        /// <inheritdoc />
+        public WellKnownController(CarWashConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/push/vapidpublickey
+        /// <summary>
+        /// Get CarWash Configuration
+        /// </summary>
+        /// <returns>CarWash Configuration</returns>
+        /// <response code="200">OK</response>
+        [HttpGet, Route("configuration")]
+        public ActionResult<WellKnown> GetConfiguration()
+        {
+            var wellKnown = new WellKnown
+            {
+                Slots = _configuration.Slots,
+                Companies = _configuration.Companies,
+                Garages = _configuration.Garages,
+                ReservationSettings = _configuration.Reservation,
+            };
+
+            return Ok(wellKnown);
+        }
+    }
+}
