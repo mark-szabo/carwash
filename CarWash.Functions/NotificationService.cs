2ec631803a4f546f089c67b982978e7337ced2a0
Mark Szabo <a-marks@microsoft.com>
2019-07-27 12:51:59 +0200

    Azure Function for integration with the Park

---
+++ CarWash.Functions/NotificationService.cs
@@ -0,0 +1,62 @@
+﻿using CarWash.ClassLibrary.Extensions;
+using CarWash.ClassLibrary.Models;
+using CarWash.ClassLibrary.Models.ServiceBus;
+using CarWash.ClassLibrary.Services;
+using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash.Functions
+{
+    class NotificationService
+    {
+        public static async Task SendEmailReminder(Reservation reservation, Email email)
+        {
+            try { await email.Send(); }
+            catch (Exception e)
+            {
+                throw new Exception($"Failed to send email to user with id: {reservation.UserId}. See inner exception.", e);
+            }
+        }
+
+        public static async Task SendPushReminder(Reservation reservation, IPushDbContext context, Notification notification)
+        {
+            var vapidSubject = Environment.GetEnvironmentVariable("VapidSubject", EnvironmentVariableTarget.Process);
+            var vapidPublicKey = Environment.GetEnvironmentVariable("VapidPublicKey", EnvironmentVariableTarget.Process);
+            var vapidPrivateKey = Environment.GetEnvironmentVariable("VapidPrivateKey", EnvironmentVariableTarget.Process);
+
+            var pushService = new PushService(context, vapidSubject, vapidPublicKey, vapidPrivateKey);
+
+            try { await pushService.Send(reservation.UserId, notification); }
+            catch (Exception e)
+            {
+                throw new Exception($"Failed to send push to user with id: {reservation.UserId}. See inner exception.", e);
+            }
+        }
+
+        public static async Task SendBotReminderMessage(Reservation reservation, string queueName)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ServiceBus", EnvironmentVariableTarget.Process);
+            var queueClient = new QueueClient(connectionString, queueName);
+
+            var message = new ReservationServiceBusMessage
+            {
+                UserId = reservation.UserId,
+                ReservationId = reservation.Id,
+            };
+
+            // Create a new message to send to the queue.
+            var serviceBusMessage = new Message(Encoding.UTF8.GetBytes(message.ToJson()));
+
+            // Send the message to the queue.            
+            try { await queueClient.SendAsync(serviceBusMessage); }
+            catch (Exception e)
+            {
+                throw new Exception($"Failed to send message to the bot, who would have pinged the user with id: {reservation.UserId}. See inner exception.", e);
+            }
+
+            await queueClient.CloseAsync();
+        }
+    }
+}
--- CarWash.Functions/ReminderFunction.cs
+++ CarWash.Functions/ReminderFunction.cs
@@ -1,5 +1,4 @@
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.ServiceBus;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,17 +6,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Text;
 using CarWash.ClassLibrary.Enums;
 using CarWash.ClassLibrary.Models;
-using CarWash.ClassLibrary.Services;
-using CarWash.ClassLibrary.Models.ServiceBus;
-using CarWash.ClassLibrary.Extensions;
 
 namespace CarWash.Functions
 {
     public static class ReminderFunction
     {
+        private static readonly FunctionsDbContext _context = new FunctionsDbContext();
+
         /// <summary>
         /// Number of minutes before a reservation after we should send the reminder
         /// The function is running every 10 minutes, so use a number not dividable with 10 for consistent execution.
@@ -43,8 +40,7 @@ namespace CarWash.Functions
             var watch = Stopwatch.StartNew();
 
             // Get reservations from SQL database
-            var context = new FunctionsDbContext();
-            var reservations = await context.Reservation
+            var reservations = await _context.Reservation
                 .Include(r => r.User)
                 .Where(r => r.State == State.SubmittedNotActual && r.StartDate.Date == DateTime.Today)
                 .ToListAsync();
@@ -65,10 +61,20 @@ namespace CarWash.Functions
             foreach (var reservation in reservations)
             {
                 reservation.State = State.ReminderSentWaitingForKey;
-                await context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
                 log.LogInformation($"Reservation state updated to ReminderSentWaitingForKey for reservation with id: {reservation.Id}. ({watch.ElapsedMilliseconds}ms)");
 
                 if (reservation.User == null) throw new Exception($"Failed to load user with id: {reservation.UserId}");
+
+                var email = new Email
+                {
+                    To = reservation.User.Email,
+                    Subject = "CarWash reminder",
+                    Body = $@"Hi {reservation.User.FirstName}, 
+It's time to leave the key at the reception and <a href='https://carwashu.azurewebsites.net'>confirm drop-off & vehicle location by clicking here</a>!
+
+If don't want to get email reminders in the future, you can <a href='https://carwashu.azurewebsites.net/settings'>disable it in the settings</a>."
+                };
 
                 switch (reservation.User.NotificationChannel)
                 {
@@ -77,19 +83,30 @@ namespace CarWash.Functions
                         break;
                     case NotificationChannel.NotSet:
                     case NotificationChannel.Email:
-                        await SendEmailReminder(reservation);
+                        await NotificationService.SendEmailReminder(reservation, email);
                         log.LogInformation($"Email notification was sent to the user ({reservation.User.Id}) about the reservation with id: {reservation.Id}. ({watch.ElapsedMilliseconds}ms)");
                         break;
                     case NotificationChannel.Push:
                         try
                         {
-                            await SendPushReminder(reservation, context);
+                            var notification = new Notification
+                            {
+                                Title = "CarWash reminder",
+                                Body = "It's time to leave the key at the reception and confirm vehicle location!",
+                                Tag = NotificationTag.Reminder,
+                                RequireInteraction = true,
+                                Actions = new List<NotificationAction>
+                                {
+                                    new NotificationAction { Action = "confirm-dropoff", Title = "Confirm drop-off" }
+                                }
+                            };
+                            await NotificationService.SendPushReminder(reservation, _context, notification);
                             log.LogInformation($"Push notification was sent to the user ({reservation.User.Id}) about the reservation with id: {reservation.Id}. ({watch.ElapsedMilliseconds}ms)");
                         }
                         catch (Exception e)
                         {
                             log.LogError(e, "Push notification cannot be sent. Failover to email.");
-                            await SendEmailReminder(reservation);
+                            await NotificationService.SendEmailReminder(reservation, email);
                             log.LogInformation($"Email notification was sent to the user ({reservation.User.Id}) about the reservation with id: {reservation.Id}. ({watch.ElapsedMilliseconds}ms)");
                         }
 
@@ -99,82 +116,12 @@ namespace CarWash.Functions
                 }
 
                 // Send message to ServiceBus which is monitored by the bot, who will ping the user
-                await SendBotReminderMessage(reservation);
+                await NotificationService.SendBotReminderMessage(reservation, BotReminderQueueName);
                 log.LogInformation($"Bot reminder was sent to the user ({reservation.User.Id}) about the reservation with id: {reservation.Id}. ({watch.ElapsedMilliseconds}ms)");
             }
 
             log.LogInformation("All reminders have been sent successfully.");
             watch.Stop();
-        }
-
-        private static async Task SendEmailReminder(Reservation reservation)
-        {
-            var email = new Email
-            {
-                To = reservation.User.Email,
-                Subject = "CarWash reminder",
-                Body = $@"Hi {reservation.User.FirstName}, 
-It's time to leave the key at the reception and <a href='https://carwashu.azurewebsites.net'>confirm drop-off & vehicle location by clicking here</a>!
-
-If don't want to get email reminders in the future, you can <a href='https://carwashu.azurewebsites.net/settings'>disable it in the settings</a>."
-            };
-
-            try { await email.Send(); }
-            catch (Exception e)
-            {
-                throw new Exception($"Failed to send email to user with id: {reservation.UserId}. See inner exception.", e);
-            }
-        }
-
-        private static async Task SendPushReminder(Reservation reservation, IPushDbContext context)
-        {
-            var vapidSubject = Environment.GetEnvironmentVariable("VapidSubject", EnvironmentVariableTarget.Process);
-            var vapidPublicKey = Environment.GetEnvironmentVariable("VapidPublicKey", EnvironmentVariableTarget.Process);
-            var vapidPrivateKey = Environment.GetEnvironmentVariable("VapidPrivateKey", EnvironmentVariableTarget.Process);
-
-            var pushService = new PushService(context, vapidSubject, vapidPublicKey, vapidPrivateKey);
-
-            var notification = new Notification
-            {
-                Title = "CarWash reminder",
-                Body = "It's time to leave the key at the reception and confirm vehicle location!",
-                Tag = NotificationTag.Reminder,
-                RequireInteraction = true,
-                Actions = new List<NotificationAction>
-                {
-                    new NotificationAction { Action = "confirm-dropoff", Title = "Confirm drop-off" }
-                }
-            };
-
-            try { await pushService.Send(reservation.UserId, notification); }
-            catch (Exception e)
-            {
-                throw new Exception($"Failed to send push to user with id: {reservation.UserId}. See inner exception.", e);
-            }
-        }
-
-        private static async Task SendBotReminderMessage(Reservation reservation)
-        {
-            var connectionString = Environment.GetEnvironmentVariable("ServiceBus", EnvironmentVariableTarget.Process);
-            var queueClient = new QueueClient(connectionString, BotReminderQueueName);
-
-            var message = new ReservationServiceBusMessage
-            {
-                UserId = reservation.UserId,
-                ReservationId = reservation.Id,
-            };
-
-            // Create a new message to send to the queue.
-            var serviceBusMessage = new Message(Encoding.UTF8.GetBytes(message.ToJson()));
-
-            // Send the message to the queue.            
-            try { await queueClient.SendAsync(serviceBusMessage); }
-            catch (Exception e)
-            {
-                throw new Exception($"Failed to send message to the bot, who would have pinged the user with id: {reservation.UserId}. See inner exception.", e);
-            }
-
-            await queueClient.CloseAsync();
         }
     }
 }
