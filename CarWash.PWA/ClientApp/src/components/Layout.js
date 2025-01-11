70374ed60684310ac2c639ae05d420fcc0bb1d84
Mark Szabo <3098994+mark-szabo@users.noreply.github.com>
2025-01-11 10:46:07 +0100

    Redesign search on AppBar

--- CarWash.PWA/ClientApp/src/components/Layout.js
+++ CarWash.PWA/ClientApp/src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@mui/styles';
+import { styled, alpha } from '@mui/material/styles';
 import Drawer from '@mui/material/Drawer';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -104,6 +105,49 @@ const styles = theme => ({
         fontSize: '0.8125rem',
     },
 });
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    marginRight: theme.spacing(1),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
 
 class Layout extends React.Component {
     displayName = 'Layout';
@@ -231,17 +275,17 @@ class Layout extends React.Component {
                             {this.getNavbarName()}
                         </Typography>
                         {isCarwashAdmin && (
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <InputBase
+                            <Search>
+                                <SearchIconWrapper>
+                                    <SearchIcon />
+                                </SearchIconWrapper>
+                                <StyledInputBase
                                     placeholder="Search…"
                                     onChange={handleSearchChange}
                                     value={searchTerm}
-                                    style={{ color: 'inherit', marginRight: '16px' }}
+                                    inputProps={{ 'aria-label': 'search' }}
                                 />
-                                <IconButton aria-label="search" size="large">
-                                    <SearchIcon />
-                                </IconButton>
-                            </div>
+                            </Search>
                         )}
                         {refresh && (
                             <IconButton aria-label="refresh" onClick={refresh} size="large">
