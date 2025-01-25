597fba884d807776a15ebe2212ffc6311e6eb3be
Mark Szabo <3098994+mark-szabo@users.noreply.github.com>
2025-01-25 21:48:51 +0100

    Add Comment and CommentRole models to represent comments in the reservation system

---
+++ CarWash.ClassLibrary/Enums/CommentRole.cs
@@ -0,0 +1,20 @@
+﻿using CarWash.ClassLibrary.Models;
+
+namespace CarWash.ClassLibrary.Enums
+{
+    /// <summary>
+    /// Enum representing the role of the comment author.
+    /// </summary>
+    public enum CommentRole
+    {
+        /// <summary>
+        /// Message was sent by the user.
+        /// </summary>
+        User,
+
+        /// <summary>
+        /// Message was sent by a CarWash employee. User id is stored in the <see cref="Comment.UserId"/> property.
+        /// </summary>
+        Carwash
+    }
+}
