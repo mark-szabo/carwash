31953cf926b6c5210d4913659b29cf1434d4dfc8
Mark Szabo <3098994+mark-szabo@users.noreply.github.com>
2025-01-25 23:57:51 +0100

    DB MIGRATION: Comments to JSON

--- CarWash.ClassLibrary/Migrations/20250125213512_Comments.Designer.cs
+++ CarWash.ClassLibrary/Migrations/20250125213512_Comments.Designer.cs
@@ -4,6 +4,7 @@ using CarWash.ClassLibrary.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarWash.ClassLibrary.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250125213512_Comments")]
+    partial class Comments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
