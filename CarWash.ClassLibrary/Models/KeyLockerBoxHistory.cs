22ef94d7712e438e86af245c6f16fee4def34880
Mark Szabo <3098994+mark-szabo@users.noreply.github.com>
2025-07-01 11:58:00 +0200

    Refactor KeyLockerBoxHistory class to separate default constructor and parameterized constructor

--- CarWash.ClassLibrary/Models/KeyLockerBoxHistory.cs
+++ CarWash.ClassLibrary/Models/KeyLockerBoxHistory.cs
@@ -7,40 +7,62 @@ namespace CarWash.ClassLibrary.Models
     /// Audit table storing KeyLockerBox state history.
     /// DB mapped entity.
     /// </summary>
-    public class KeyLockerBoxHistory(KeyLockerBox box) : ApplicationDbContext.IEntity
+    public class KeyLockerBoxHistory : ApplicationDbContext.IEntity
     {
+        /// <summary>
+        /// Default constructor for EF and serialization.
+        /// </summary>
+        public KeyLockerBoxHistory()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new KeyLockerBoxHistory from a KeyLockerBox.
+        /// </summary>
+        /// <param name="box">The KeyLockerBox to copy values from.</param>
+        public KeyLockerBoxHistory(KeyLockerBox box)
+        {
+            Id = box.Id;
+            LockerId = box.LockerId;
+            Name = box.Name;
+            State = box.State;
+            IsDoorClosed = box.IsDoorClosed;
+            ModifiedAt = DateTime.UtcNow;
+            ModifiedBy = box.LastModifiedBy;
+        }
+
         /// <inheritdoc />
-        public string Id { get; set; } = box.Id;
+        public string Id { get; set; }
 
         /// <summary>
         /// Id of the key locker, containing multiple boxes.
         /// Device id of the IoT Hub device.
         /// </summary>
-        public string LockerId { get; set; } = box.LockerId;
+        public string LockerId { get; set; }
 
         /// <summary>
         /// Friendly name of the box, used to identify it.
         /// </summary>
-        public string Name { get; set; } = box.Name;
+        public string Name { get; set; }
 
         /// <summary>
         /// Curent state of the box.
         /// </summary>
-        public KeyLockerBoxState State { get; set; } = box.State;
+        public KeyLockerBoxState State { get; set; }
 
         /// <summary>
         /// Indicates if the box door is currently closed.
         /// </summary>
-        public bool IsDoorClosed { get; set; } = box.IsDoorClosed;
+        public bool IsDoorClosed { get; set; }
 
         /// <summary>
         /// Gets or sets the date and time when the entity was last modified.
         /// </summary>
-        public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
+        public DateTime ModifiedAt { get; set; }
 
         /// <summary>
         /// Gets or sets the user who last modified the entity.
         /// </summary>
-        public string? ModifiedBy { get; set; } = box.LastModifiedBy;
+        public string? ModifiedBy { get; set; }
     }
 }
