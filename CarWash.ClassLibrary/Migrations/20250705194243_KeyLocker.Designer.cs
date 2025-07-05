f154bb9b36048468ae32ce72d09ed3f2896511ce
Mark Szabo <3098994+mark-szabo@users.noreply.github.com>
2025-07-05 21:48:17 +0200

    DB MIGRATION: Key Locker Box

--- CarWash.ClassLibrary/Migrations/20250705194243_KeyLocker.Designer.cs
+++ CarWash.ClassLibrary/Migrations/20250705194243_KeyLocker.Designer.cs
@@ -4,6 +4,7 @@ using CarWash.ClassLibrary.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarWash.ClassLibrary.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250705194243_KeyLocker")]
+    partial class KeyLocker
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
